722a46a1f27d664573f45468326eb14aae10c907
unknown <Toheebafolabi17@gmail.com>
2021-12-03 04:53:40 -0800

    final commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@
 import { Routes, Route, Outlet } from "react-router";
 
 import CancelData from "./pages/delete";
+import DesForm from './pages/destination';
+import RegisterData from './pages/get';
+import LocationData from './pages/location';
+import LocForm from './pages/status';
+import WelcomePage from "./pages/main";
 
 
 import AdminParcel from "./parcels/Admin1";
@@ -27,6 +32,18 @@ function App() {
         <Route path='/receive' element={< ReceiveAll />}  />
 
         <Route path='/cancel' element={< CancelData />}  />
+
+        <Route index  element={< WelcomePage />}  />
+       
+
+        <Route path='/get' element={<RegisterData />} />
+
+        <Route path='/destination' element={<DesForm />} />
+
+        <Route path='/location' element={<LocationData />} />
+
+        <Route path='/status' element={<LocForm />} />
+
         
       </Routes>
       <Outlet />
--- src/index.js
+++ src/index.js
@@ -4,18 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import App from './App';
 import './index.css';
-import DesForm from './pages/destination';
-import RegisterData from './pages/get';
-import LocationData from './pages/location';
-import LocForm from './pages/status';
-import WelcomePage from "./pages/main";
-import CancelData from "./pages/delete";
 
-import AdminParcel from "./parcels/Admin1";
-import ReceiveAll from "./parcels/getall";
-import LoginParcel from "./parcels/login";
-import PostParcel from "./parcels/post";
-import RegisterParcel from "./parcels/register";
 
 
 
@@ -23,34 +12,12 @@ import RegisterParcel from "./parcels/register";
 
 
 ReactDOM.render(
-  <BrowserRouter>
+  <BrowserRouter basename={window.location.pathname || ''} >
     <Routes>
 
-      <Route path= '/'  element={<App />}  />
+      <Route exact path= '/'  element={<App />}  />
         
-        <Route index  element={< WelcomePage />}  />
        
-
-        <Route path='/get' element={<RegisterData />} />
-
-        <Route path='/destination' element={<DesForm />} />
-
-        <Route path='/location' element={<LocationData />} />
-
-        <Route path='/status' element={<LocForm />} />
-
-        <Route path='/admin' element={<AdminParcel />} />
-
-        <Route path='/login' element={< LoginParcel />}  />
-
-        <Route path='/register' element={< RegisterParcel />}  />
-
-        <Route path='/post' element={< PostParcel />}  />
-
-        <Route path='/receive' element={< ReceiveAll />}  />
-
-        <Route path='/cancel' element={< CancelData />}  />
-        
       
 
     </Routes>
